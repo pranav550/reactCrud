bef67740965e785902a1f5242acda589d252b692
pranav550 <pranavverma550>
2020-04-01 11:25:52 +0530

    git crud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Home from './componenets/pages/Home';
+import About from './componenets/pages/About';
+import Contact from './componenets/pages/Contact';
+import Navbar from './componenets/layout/navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NotFound from './componenets/pages/NotFound';
+import AddUser from './componenets/Users/AddUser';
+import EditUser from './componenets/Users/EditUser';
+import User from './componenets/Users/User';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+     <Navbar/>
+     <Switch>
+       <Route exact path="/" component={Home}/>
+       <Route exact path="/about" component={About}/>
+       <Route exact path="/contact" component={Contact}/>
+       <Route exact path="/users/add" component={AddUser}/>
+       <Route exact path="/users/edit/:id" component={EditUser}/>
+       <Route exact path="/users/:id" component={User}/>
+       <Route component={NotFound}/>
+     </Switch>
+     
+       </Router>
     </div>
   );
 }
---
+++ src/componenets/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const About= () =>{
+    return(
+        <div className="container">
+            <div className="py-4">
+                <h1>About Page</h1>
+                <p className="lead">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                </p>
+                <p className="lead">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                </p>
+                <p className="lead">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
